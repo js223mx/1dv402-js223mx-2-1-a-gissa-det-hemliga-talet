54351cfb607dac0406537ba98598594960529c82
js223mx <js223mx@student.lnu.se>
2014-10-16 11:39:24 +0200

    done with secretnumber

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L1A
 {
-    class SecretNumber
+    public class SecretNumber
     {
         private int _count;
         private int _number;
-        const int MaxNumberOfGuesses = 7;
+        public const int MaxNumberOfGuesses = 7;
 
         public SecretNumber() //Konstruktor
         {
@@ -30,23 +30,33 @@ namespace _1DV402.S2.L1A
             {
                 throw new ApplicationException();
             }
+
             if (number < 1 || number > 100)
             {
-                throw new ArgumentException();
+                throw new ArgumentOutOfRangeException();
             }
+            
             ++_count;
+
             if (number < _number)
             {
                 Console.WriteLine("Talet {0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
             }
-            else 
+            else
             {
                 Console.WriteLine("Talet {0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
             }
             if (number == _number)
             {
                 Console.WriteLine("Rätt gissat! Du klarade det på {0} försök.", _count);
+                return true;
             }
+            if (_count == MaxNumberOfGuesses)
+            {
+                Console.WriteLine("Det hemliga talet är {0}.", _number);
+            }
+            // return false eftersom det inte var lika mycket som det hemliga värdet
+            return false;
         }
 
      }
